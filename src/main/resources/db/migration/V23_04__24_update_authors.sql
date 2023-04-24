0c847666bb51f0db2d6bc97d0f6e2db50bf2319f
yuvadany <toyuvaraj@gmail.com>
2023-04-24 22:10:38 +0200

    Flyway migration - ignoring missed migration

---
+++ src/main/resources/db/migration/V23_04__24_update_authors.sql
@@ -0,0 +1,8 @@
+INSERT INTO `springdb`.`authors` (`id`, `author_name`) VALUES ('14', 'Author_14');
+INSERT INTO `springdb`.`authors` (`id`, `author_name`) VALUES ('15', 'Author_15');
+INSERT INTO `springdb`.`authors` (`id`, `author_name`) VALUES ('16', 'Author_16');
+INSERT INTO `springdb`.`authors` (`id`, `author_name`) VALUES ('17', 'Author_17');
+INSERT INTO `springdb`.`authors` (`id`, `author_name`) VALUES ('18', 'Author_18');
+INSERT INTO `springdb`.`authors` (`id`, `author_name`) VALUES ('19', 'Author_19');
+INSERT INTO `springdb`.`authors` (`id`, `author_name`) VALUES ('20', 'Author_20');
+INSERT INTO `springdb`.`authors` (`id`, `author_name`) VALUES ('21', 'Author_21');
